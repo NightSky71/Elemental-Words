7b26448142fec029d86a53c68e00640627232409
George Lowe <george.lowe94@gmail.com>
2024-10-16 18:02:51 +0100

    Added basic console interaction

--- src/ElementalWords/Program.cs
+++ src/ElementalWords/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ElementalWords.Services;
+
+Console.WriteLine("Elemental Words");
+
+var elementalWordService = new ElementalWordService();
+
+while (true)
+{
+    Console.Write("Input Word: ");
+    var input = Console.ReadLine();
+
+    var result = elementalWordService.GetElementWords(input);
+
+    foreach (var elementalWord in result)
+    {
+        Console.WriteLine(string.Join(',', elementalWord));
+    }
+}
